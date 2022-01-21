69ac3d8661c2edcd3871ce06f4117e2f2a0a7a48
Ирина <Yakubenko1812@gmail.com>
2022-01-21 17:48:43 +0200

    update package.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/shortener'));
+app.use(express.static(__dirname + '/dist/url-shortener'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/shortener/index.html'));});
+    '/dist/url-shortener/index.html'));});
 app.listen(process.env.PORT || 8080);
